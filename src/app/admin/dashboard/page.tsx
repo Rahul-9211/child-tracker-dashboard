8accc27ab57dc15c11e12fc9cd7268dcf2a0fcce
rahul-9211 <rahul.rawat.fec@gmail.com>
2025-03-21 23:54:36 +0530

    CHANGES : dashboard

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import { ThemeToggle } from "@/components/theme-toggle"
 import { AppSidebar } from "@/components/ui/app-sidebar"
 import {
   Breadcrumb,
@@ -23,7 +22,7 @@ export default function AdminDashboard() {
     <ProtectedRoute allowedRoles={["admin"]}>
       <SidebarProvider>
         <AppSidebar />
-        {/* <ThemeToggle /> */}
+        {/* add themeToggle here  */}
         <SidebarInset>
           <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
             <div className="flex items-center gap-2 px-4">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { ThemeToggle } from "@/components/theme-toggle";
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
 import { useRouter } from "next/navigation";
 export default function Home() {
   const router = useRouter();
